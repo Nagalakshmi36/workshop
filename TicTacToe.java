f080c82f58e63bae4bcf66e9e348e49996f9d934
nagalakshmi <nagalakshmiobillaneni1501@gmail.com>
2020-11-19 11:21:29 +0530

    initialize board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,19 @@
 import java.io.*;
-public class TicTacToe{
+public class TicTacToe
+{
 	static char[] creatingBoard()
 	{
-			board char[]=new char[10];
+			char[] board=new char[10];
 				for(int index=1;index<board.length;index++)
 				{
 						board[index];
 				}
 				return board;
 	}
-}
+
 public static void main(String args[])
 {
 	System.out.println("Tic Tac Toe Game");
 	creatingBoard();
 }
+
