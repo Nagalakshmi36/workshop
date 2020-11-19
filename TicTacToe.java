7ed17d887fb7d1352083a857dba2302325fdf243
nagalakshmi <nagalakshmiobillaneni1501@gmail.com>
2020-11-19 10:27:38 +0530

    create board

---
+++ TicTacToe.java
@@ -0,0 +1,16 @@
+impiort java.io.*;
+public class TicTacToe
+{
+	public static void main(String args[])
+	{
+		TicTacToe tic=new TicTacToe();
+		char[][] board;
+		for(int check=0;check<10;check++)
+		{
+			for(int check2=0;check2<10;check++)
+			{
+				board[check][check2]='';
+			}
+		}
+	}
+}
